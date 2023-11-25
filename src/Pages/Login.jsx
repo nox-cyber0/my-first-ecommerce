7220484e114959ef7d6ef04292a8bb372be1d75a
nox-cyber0 <mhabubakar999@outlook.com>
2023-11-25 00:56:05 +0000

    updates

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,19 +4,35 @@ import { useAuth } from "../Utils/AuthContext";
 // import useAuth from "../Utils/useAuth";
 
 const Login = () => {
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+
   return (
     <>
       <div className="flex flex-wrap h-screen">
         <div className="flex justify-center items-center h-full bg-gray-50 w-full xl:w-1/3 md:w-1/2 dark:bg-slate-800">
-          <form className="w-3/4 mx-auto">
+          <form
+            className="w-3/4 mx-auto"
+            onSubmit={handleSubmit}>
             <h1 className="text-2xl font-semibold mb-6 text-slate-800 dark:text-white">Log in to continue</h1>
             <div className="mb-4">
               <label htmlFor="username" className="block text-left text-sm font-medium mb-2 text-slate-400">Username</label>
-              <input type="text" className="block w-full text-base p-2 rounded-md bg-transparent border text-slate-800 border-slate-500 focus:bg-slate-50 focus:outline-slate-800 dark:text-slate-200 dark:focus:bg-slate-700 dark:outline-slate-100" />
+              <input type="text"
+                className="block w-full text-base p-2 rounded-md bg-transparent border text-slate-800 border-slate-500 focus:bg-slate-50 focus:outline-slate-800 dark:text-slate-200 dark:focus:bg-slate-700 dark:outline-slate-100"
+                onChange={(e) => setUser({
+                  user: e.target.value
+                })
+                }
+              />
             </div>
             <div className="mb-4">
               <label htmlFor="password" className="block text-left text-sm font-medium mb-2 text-slate-400">Password</label>
-              <input type="password" className="block w-full text-base p-2 rounded-md bg-transparent border text-slate-800 border-slate-500 focus:bg-slate-50 focus:outline-slate-800 dark:text-slate-200 dark:focus:bg-slate-700 dark:outline-slate-100" />
+              <input type="password" className="block w-full text-base p-2 rounded-md bg-transparent border text-slate-800 border-slate-500 focus:bg-slate-50 focus:outline-slate-800 dark:text-slate-200 dark:focus:bg-slate-700 dark:outline-slate-100"
+                onChange={(e) => setPwd({
+                  pwd: e.target.value
+                })
+                }
+              />
             </div>
             <button className="block w-full text-center text-sm font-medium border border-slate-800 p-2 rounded-md bg-slate-200 hover:bg-slate-300 transition-all mt-3 dark:border-0 dark:bg-slate-100 dark:hover:bg-slate-200">Sign In</button>
             <p className="text-slate-400 dark:text-slate-200 font-medium text-sm mt-4">Not registered yet? <Link className="text-blue-700 transition-all hover:underline dark:text-blue-500" to="/sign-up">Create an account</Link></p>
