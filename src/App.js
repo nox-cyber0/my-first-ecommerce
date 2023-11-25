53e5d96fe03bb7b61e5ca2ed93cf4de0e2f60bd8
nox-cyber0 <140342507+nox-cyber0@users.noreply.github.com>
2023-11-25 02:58:35 +0000

    Testing dark mode

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App dark">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="products" element={<Products />} />
